74027c7f5fd07b82fb54630b58b044aac256c226
Abhishek <neoscreenager@gmail.com>
2018-11-23 12:54:36 +0530

    Added Icons to the menu

--- src/main/java/in/synergicfuturz/todolist/client/TodoListApp.java
+++ src/main/java/in/synergicfuturz/todolist/client/TodoListApp.java
@@ -2,6 +2,8 @@ package in.synergicfuturz.todolist.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.vaadin.polymer.Polymer;
+import com.vaadin.polymer.elemental.Function;
 
 public class TodoListApp implements EntryPoint {
   public void onModuleLoad() {
@@ -9,6 +11,18 @@ public class TodoListApp implements EntryPoint {
     /*PaperButton button = new PaperButton("Press me!");
     button.setRaised(true);
     RootPanel.get().add(button);*/
+	// We have to load icon sets before run application
+	    Polymer.importHref("iron-icons/iron-icons.html", new Function() {
+	        public Object call(Object arg) {
+	            // The app is executed when all imports succeed.
+	            startApplication();
+	            return null;
+	        }
+	    });
+	  //RootPanel.get().add(new Main());
+  }
+  private void startApplication() {
+	  //ssRootPanel.get().clear();
 	  RootPanel.get().add(new Main());
   }
 }
